1ed084b75be94f7979cda74309d3d30058514cc6
Nicofolxarus <nicobis@orange.fr>
2018-05-10 16:20:53 +0200

    first commit

---
+++ src/fr/NS/Labyrinthev2/Generale/Main.java
@@ -0,0 +1,24 @@
+package fr.NS.Labyrinthev2.Generale;
+
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin implements Listener {
+	
+	@Override
+	public void onLoad() {
+		super.onLoad();
+	}
+	
+	@Override
+	public void onEnable() {
+		super.onEnable();
+	}
+	
+	@Override
+	public void onDisable() {
+		super.onDisable();
+	}
+	
+	
+}
